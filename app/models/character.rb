177465573142983d9f5f1e6c13766232c5b99484
Matt Taube <mctaube@users.noreply.github.com>
2017-05-17 00:02:54 -0500

    some updates

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,11 +1,14 @@
 class Character < ApplicationRecord
 
   #  - movie_id: must be present
-validates :movie_id, :presence => true
+  validates :movie_id, :presence => true
 
   #  - actor_id: must be present
-validates :actor_id, :presence => true
+  validates :actor_id, :presence => true
 
   #  - name: no rules
 
+  has_many(:movies, :class_name => "Movie", :foreign_key => "actor_id")
+  has_many(:movies, :class_name => "Movie", :foreign_key => "movie_id")
+  
 end
